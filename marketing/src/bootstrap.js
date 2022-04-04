065e1f61c284e7ceda65e5fac03b139a2ef05f68
Chandra Sekhar Veera <veeracs@gmail.com>
2022-04-03 22:16:13 -0400

    add landing page, pricing page, webpack dev, common config

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const mount = (el) => {
+    ReactDOM.render(
+        <App />,
+        el
+    )
+};
+
+if (process.env.NODE_ENV === 'development') {
+    const marketingDevRoot = document.querySelector('#marketing-dev-root');
+
+    if (marketingDevRoot) {
+        mount(marketingDevRoot);
+    }
+}
+
+export { mount };
