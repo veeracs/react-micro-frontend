4de1f5deec808087f3214ce409510176daaa1224
Chandra Sekhar Veera <veeracs@gmail.com>
2022-04-26 22:24:57 -0400

    MF to Container communication

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -4,8 +4,12 @@ import { createMemoryHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el) => {
+const mount = (el, { onNavigate }) => {
   const history = createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
 
   ReactDOM.render(<App history={history} />, el);
 };
@@ -16,7 +20,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {});
   }
 }
 
